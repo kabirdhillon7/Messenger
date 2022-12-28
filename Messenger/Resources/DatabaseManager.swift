40d762dacfd617238cdcb46203a86eb1829245cd
Kabir Dhillon <kabirdhillon99@gmail.com>
2022-12-27 21:56:39 -0800

    DatabaseManager
    Updated w/ safe email var to satisfy conditions

--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -21,7 +21,11 @@ final class DatabaseManager {
 extension DatabaseManager {
     public func userExists(with email: String,
                            completion: @escaping ((Bool) -> Void)) {
-        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
             guard snapshot.value as? String != nil else {
                 completion(false)
                 return
@@ -35,7 +39,7 @@ extension DatabaseManager {
     /// Inserts new user to database
     public func insertUser(with user: ChatAppUser) {
         // Delimiting users by their email
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
         "first_name": user.firstName,
         "last_name": user.lastName])
     }
@@ -46,4 +50,12 @@ struct ChatAppUser {
     let lastName: String
     let emailAddress: String
     //let profilePictureUrl: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        return safeEmail
+    }
+    
 }

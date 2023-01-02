956889be1b139008dec77197bb016b4953e5d27e
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-01-01 21:22:12 -0800

    ConvoVC
    Added tableView and spinner

--- Messenger/Controller/ConversationsViewController.swift
+++ Messenger/Controller/ConversationsViewController.swift
@@ -7,11 +7,36 @@
 
 import UIKit
 import FirebaseAuth
+import JGProgressHUD
 
 class ConversationsViewController: UIViewController {
+    
+    private let spinner = JGProgressHUD(style: .dark)
 
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.isHidden = true
+        return table
+    }()
+    
+    private let noConversationsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "No Conversations"
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 21, weight: .medium)
+        label.isHidden = true
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(tableView)
+        view.addSubview(noConversationsLabel)
+        setupTableView()
+        fetchConversations()
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -28,6 +53,36 @@ class ConversationsViewController: UIViewController {
             present(nav, animated: false)
         }
     }
+    
+    private func setupTableView() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func fetchConversations() {
+        
+    }
 
+}
+
+extension ConversationsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "hello world"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
 }
 

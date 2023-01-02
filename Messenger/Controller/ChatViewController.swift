1310fce00235ac7a577fea564afabd520e6fec87
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-01-01 21:30:36 -0800

    Added JGProgressHUD Spinner to Login and Register VCs

---
+++ Messenger/Controller/ChatViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatViewController.swift
+//  Messenger
+//
+//  Created by Kabir Dhillon on 1/1/23.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+
+}
--- Messenger/Controller/Login/LoginViewController.swift
+++ Messenger/Controller/Login/LoginViewController.swift
@@ -9,8 +9,11 @@ import UIKit
 import FirebaseAuth
 import FBSDKLoginKit
 import GoogleSignIn
+import JGProgressHUD
 
 class LoginViewController: UIViewController {
+    
+    private let spinner = JGProgressHUD(style: .dark)
     
     private let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
@@ -161,6 +164,8 @@ class LoginViewController: UIViewController {
                 return
         }
         
+        spinner.show(in: view)
+        
         // Firebase Login
         FirebaseAuth.Auth.auth().signIn(withEmail: email,
                                         password: password,
@@ -168,6 +173,11 @@ class LoginViewController: UIViewController {
             guard let strongSelf = self else {
                 return
             }
+            
+            DispatchQueue.main.async {
+                strongSelf.spinner.dismiss()
+            }
+            
             guard let result = authResult, error == nil else {
                 print("Failed to login user with email: \(email)")
                 return
